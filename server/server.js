f3ffa88303b787e4ee6b6647551a641384e92946
Manish <man8872pal@gmail.com>
2025-07-25 23:14:47 +0530

    changing configuration

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ import connectDB from './config/mongodb.js';
 import userRouter from './routes/userRoute.js';
 import imageRouter from './routes/imageRoute.js';
 
-const PORT = process.env.PORT || 4000;
+
 const app = express();
 
 app.use(express.json());
@@ -18,4 +18,10 @@ app.get('/', (req, res) => {
     res.send("API Working");
 })
 
+// if statement is initiazlized to deploy the project on vercel
+if(process.env.NODE_ENV !== 'production') {
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log('Server is running on port ' + PORT ));
+}
+//export default server for vercel
+export default server;
